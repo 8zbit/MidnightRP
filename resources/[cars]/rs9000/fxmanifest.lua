159be8441d05dfe44ba621145fd172f83039a37f
rdephillip <rdephillip@gmail.com>
2020-03-21 20:28:19 -0400

    Resource Upload [4]
    Resource Upload

---
+++ resources/[cars]/rs9000/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+ui_page 'nui/index.html'
+
+files {
+	'config.json',
+	'nui/index.html',
+	'nui/index.js',
+	'nui/style.css',
+	'nui/d-din.otf',
+	'nui/dseg7.ttf',
+	'Newtonsoft.Json.dll',
+}
+
+client_script 'RS9000.net.dll'
