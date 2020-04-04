af433d42ed4f92e1967dad991a4076e7cacbfd56
kamelzarandah <kamelzarandah92@gmail.com>
2020-04-04 06:06:14 +0300

    Changed location of coke processing
    Changed location of coke processing

--- resources/[esx]/esx_drugs/config.lua
+++ resources/[esx]/esx_drugs/config.lua
@@ -61,7 +61,7 @@ Config.CircleZones = {
 Config.ProcessZones = {
 
 	WeedProcessing = {coords = vector3(1035.68, -3204.92, -38.17)--[[, name = _U('blip_weedprocessing'), color = 25, sprite = 496, radius = 100.0]]},
-	CokeProcessing = {coords = vector3(1437.55, -1490.03, 66.62)--[[, name = _U('blip_cokeprocessing'), color = 30, sprite = 501, radius = 100.0]]},
+	CokeProcessing = {coords = vector3(-1870.42, 2061.16, 135.43)--[[, name = _U('blip_cokeprocessing'), color = 30, sprite = 501, radius = 100.0]]},
 	SpiceProcessing = {coords = vector3(1257.19, 3133.56, -72.31)--[[, name = _U('blip_spiceprocessing'), color = 30, sprite = 501, radius = 100.0]]},
 }
 
@@ -74,7 +74,7 @@ Config.MarkerZones = {
 
 	{x = 2675.84,y = 3499.31,z = 52.5}, ---sales
 	{x = 1035.68,y = -3204.92,z = -38.17}, ---weed prosessing
-	{x = 1437.55,y = -1490.03,z =  66.62}, ---coke
+	{x = -1870.42,y = 2061.16,z =  135.43}, ---coke
 	{x = 1257.19,y = 3133.56,z = -74.31}, ----spice
 
 
