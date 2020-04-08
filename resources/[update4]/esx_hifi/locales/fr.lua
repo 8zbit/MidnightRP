c30d5aafd075087c43b8e2146ad138e0e380e727
kamelzarandah <62485293+kamelzarandah@users.noreply.github.com>
2020-04-08 12:03:17 +0300

    translation of boombox
    translation of boombox

--- resources/[update4]/esx_hifi/locales/fr.lua
+++ resources/[update4]/esx_hifi/locales/fr.lua
@@ -1,12 +1,12 @@
 Locales ['fr'] = {
-	['put_hifi'] = 'Vous venez de deposer la BoomBox',
-	['get_hifi'] = 'Ramasser la BoomBox',
-	['play_music'] = 'Jouer une musique',
-	['stop_music'] = 'Stopper la musique',
-	['volume_music'] = 'Regler le son',
-	['hifi_alreadyOne'] = 'Vous avez deja une BoomBox sur vous',
-	['set_volume'] = 'Entrez le niveau du volume (entre 0 et 100)',
-	['play_id'] = 'Entrez l\'id de la video Youtube',
-	['sound_limit'] = 'Le volume doit etre entre 0 et 100',
-	['hifi_help'] = 'appuyez sur ~INPUT_CONTEXT~ pour regler la musique'
+	['put_hifi'] = 'You have just dropped the BoomBox',
+	['get_hifi'] = 'Pick up the BoomBox',
+	['play_music'] = 'Start the music',
+	['stop_music'] = 'Stop the music',
+	['volume_music'] = 'Adjust the volume',
+	['hifi_alreadyOne'] = 'You already have a BoomBox on you',
+	['set_volume'] = 'Enter the volume level (between 0 and 100)',
+	['play_id'] = 'Enter the id of the YouTube video',
+	['sound_limit'] = 'The volume must be between 0 and 100',
+	['hifi_help'] = 'press ~ INPUT_CONTEXT ~ to adjust the music'
 }
